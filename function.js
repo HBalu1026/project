58693a21d777dbbdda91416eef3e7d75154f05cf
hernadi.balazs <hernadi.balazs@BURG-1E05-17>
2024-09-12 09:52:38 +0200

    improvement

--- function.js
+++ function.js
@@ -2,13 +2,15 @@ function getFirstElement(arr) {
     if (arr.length > 0) {
       return arr[0];
     } else {
-      return undefined;
+      return "A tömb üres";
     }
   }
   
   const numbers = [1, 2, 3];
   const words = ["apple", "banana", "cherry"];
+  const emptyArray = [];
   
   console.log(`First number: ${getFirstElement(numbers)}`);
   console.log(`First word: ${getFirstElement(words)}`);
+  console.log(`First element of empty array: ${getFirstElement(emptyArray)}`);
   
